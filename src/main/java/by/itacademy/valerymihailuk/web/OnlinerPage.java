2186f257961d28219365419395837117d365e5c6
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-02-14 00:21:53 +0300

    add onliner and onlinertest class

--- src/main/java/by/itacademy/valerymihailuk/web/OnlinerPage.java
+++ src/main/java/by/itacademy/valerymihailuk/web/OnlinerPage.java
@@ -1,4 +1,12 @@
 package by.itacademy.valerymihailuk.web;
 
 public class OnlinerPage {
+
+
+        public static final String URL = "https://www.onliner.by/";
+        public static  final String X_PATH_2001_COPYRIGTH = "/html/body/div[4]/footer/div/div/div/div[2]/div[2]";
+public  static  final  String BTN_LOGIN = "";
+public static  final  String LOGIN_LOGO = "";
+
+
 }
--- src/test/java/by/itacademy/valerymihailuk/web/OnlinerTest.java
+++ src/test/java/by/itacademy/valerymihailuk/web/OnlinerTest.java
@@ -1,4 +1,30 @@
 package by.itacademy.valerymihailuk.web;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OnlinerTest {
+    @Test
+    public void OnlinerOpenPage() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        WebElement element = driver.findElement(By.xpath(OnlinerPage.X_PATH_2001_COPYRIGTH));
+        Assert.assertTrue(element.isDisplayed());
+        driver.quit();
+    }
+
+    @Test
+    public void OnlinerOpenLoginPage() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        By btmLoginBy = By.xpath(OnlinerPage.BTN_LOGIN)
+        WebElement element = driver.findElement(By.xpath(OnlinerPage.));
+        Assert.assertTrue(element.isDisplayed());
+        driver.quit();
+    }
 }
