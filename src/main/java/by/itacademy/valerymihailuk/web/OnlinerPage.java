f88466be3126d3109919f8bab9e822ff12de4fbc
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-02-14 20:11:59 +0300

    add more code in onliner and onlinertest in class

--- src/main/java/by/itacademy/valerymihailuk/web/OnlinerPage.java
+++ src/main/java/by/itacademy/valerymihailuk/web/OnlinerPage.java
@@ -7,6 +7,9 @@ public class OnlinerPage {
     public static final String X_PATH_2001_COPYRIGTH = "//div[@ class=\"footer-style__copy\"]";
     public static final String BTN_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
     public static final String LOGIN_LOGO = "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
-
-
+    public static final String LOGIN_INPUT = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[3]/button";
+    public static final String LOGIN_NIK_NAME_ERROR = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static final String LOGIN_PASSWORD_ERROR = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+    public static final String LOGIN_NIK_NAME_INPUT ="//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[1]/div/input";
+    public static final String EMAIL = "test@gmail.com";
 }
--- src/test/java/by/itacademy/valerymihailuk/web/OnlinerTest.java
+++ src/test/java/by/itacademy/valerymihailuk/web/OnlinerTest.java
@@ -3,12 +3,13 @@ package by.itacademy.valerymihailuk.web;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
     @Test
-    public void OnlinerOpenPage() {
+    public void testOpenOnliner() {
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(OnlinerPage.URL);
@@ -18,7 +19,7 @@ public class OnlinerTest {
     }
 
     @Test
-    public void OnlinerOpenLoginPage() {
+    public void testOpenOnlinerLoginForm() {
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(OnlinerPage.URL);
@@ -27,6 +28,40 @@ public class OnlinerTest {
         btmLoginElement.click();
         WebElement textElementLogin = driver.findElement(By.xpath(OnlinerPage.LOGIN_LOGO));
         Assert.assertEquals("Вход", textElementLogin.getText());
+        driver.quit();
+    }
+
+   @Test
+    public void testOnlinerLoginFormWithEmptyCredentials() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        By btnLoginBy = By.xpath(OnlinerPage.BTN_LOGIN);
+        WebElement btmLoginElement = driver.findElement(btnLoginBy);
+        btmLoginElement.click();
+        WebElement btmLoginInput = driver.findElement(By.xpath(OnlinerPage.LOGIN_INPUT));
+        btmLoginInput.click();
+        WebElement textElementError1 = driver.findElement(By.xpath(OnlinerPage.LOGIN_NIK_NAME_ERROR));
+        Assert.assertEquals("Укажите ник или e-mail", textElementError1.getText());
+        WebElement textElementError2 = driver.findElement(By.xpath(OnlinerPage.LOGIN_PASSWORD_ERROR));
+        Assert.assertEquals("Укажите пароль", textElementError2.getText());
+        driver.quit();
+    }
+
+    @Test
+    public void testOnlinerLoginFormWithEmptyPassword() {
+       WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        By btmLoginBy = By.xpath(OnlinerPage.BTN_LOGIN);
+        WebElement btmLoginElement = driver.findElement(btmLoginBy);
+        btmLoginElement.click();
+        WebElement textLoginInput = driver.findElement(By.xpath(OnlinerPage.LOGIN_NIK_NAME_INPUT));
+        textLoginInput.sendKeys(OnlinerPage.EMAIL);
+        WebElement btmLoginInput = driver.findElement(By.xpath(OnlinerPage.LOGIN_INPUT));
+        btmLoginInput.click();
+        WebElement textElementError = driver.findElement(By.xpath(OnlinerPage.LOGIN_PASSWORD_ERROR));
+        Assert.assertEquals("Укажите пароль", textElementError.getText());
         driver.quit();
     }
 }
