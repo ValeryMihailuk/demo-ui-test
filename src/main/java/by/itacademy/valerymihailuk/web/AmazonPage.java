c5ea26802323558d969d3ea6551a7a8c5e6af108
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-02-14 20:52:19 +0300

    add amazon and amazon test class

---
+++ src/main/java/by/itacademy/valerymihailuk/web/AmazonPage.java
@@ -0,0 +1,14 @@
+package by.itacademy.valerymihailuk.web;
+
+public class AmazonPage {
+    public static final String URL = "https://www.amazon.com/";
+    public static final String TEXT_COPYRIGHT ="© 1996-2023, Amazon.com, Inc. or its affiliates";
+    public static final String X_PATH_COPYRIGTH = "/html/body/div[1]/div[5]/div[5]/span";
+    public static final String BTN_LOGIN = "]";
+    public static final String LOGIN_LOGO = "";
+    public static final String LOGIN_INPUT = "";
+    public static final String LOGIN_NIK_NAME_ERROR = "";
+    public static final String LOGIN_PASSWORD_ERROR = "";
+    public static final String LOGIN_NIK_NAME_INPUT ="";
+    public static final String EMAIL = "";
+}
---
+++ src/test/java/by/itacademy/valerymihailuk/web/AmazonTest.java
@@ -0,0 +1,68 @@
+package by.itacademy.valerymihailuk.web;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonTest {
+
+    @Test
+    public void testOpenAmazon() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(AmazonPage.URL);
+        WebElement textElement = driver.findElement(By.xpath(AmazonPage.X_PATH_COPYRIGTH));
+        Assert.assertEquals(AmazonPage.TEXT_COPYRIGHT, textElement.getText());
+        driver.quit();
+    }
+
+    @Test
+    public void testOpenOnlinerLoginForm() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        By btnLoginBy = By.xpath(OnlinerPage.BTN_LOGIN);
+        WebElement btmLoginElement = driver.findElement(btnLoginBy);
+        btmLoginElement.click();
+        WebElement textElementLogin = driver.findElement(By.xpath(OnlinerPage.LOGIN_LOGO));
+        Assert.assertEquals("Вход", textElementLogin.getText());
+        driver.quit();
+    }
+
+    @Test
+    public void testOnlinerLoginFormWithEmptyCredentials() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        By btnLoginBy = By.xpath(OnlinerPage.BTN_LOGIN);
+        WebElement btmLoginElement = driver.findElement(btnLoginBy);
+        btmLoginElement.click();
+        WebElement btmLoginInput = driver.findElement(By.xpath(OnlinerPage.LOGIN_INPUT));
+        btmLoginInput.click();
+        WebElement textElementError1 = driver.findElement(By.xpath(OnlinerPage.LOGIN_NIK_NAME_ERROR));
+        Assert.assertEquals("Укажите ник или e-mail", textElementError1.getText());
+        WebElement textElementError2 = driver.findElement(By.xpath(OnlinerPage.LOGIN_PASSWORD_ERROR));
+        Assert.assertEquals("Укажите пароль", textElementError2.getText());
+        driver.quit();
+    }
+
+    @Test
+    public void testOnlinerLoginFormWithEmptyPassword() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        By btmLoginBy = By.xpath(OnlinerPage.BTN_LOGIN);
+        WebElement btmLoginElement = driver.findElement(btmLoginBy);
+        btmLoginElement.click();
+        WebElement textLoginInput = driver.findElement(By.xpath(OnlinerPage.LOGIN_NIK_NAME_INPUT));
+        textLoginInput.sendKeys(OnlinerPage.EMAIL);
+        WebElement btmLoginInput = driver.findElement(By.xpath(OnlinerPage.LOGIN_INPUT));
+        btmLoginInput.click();
+        WebElement textElementError = driver.findElement(By.xpath(OnlinerPage.LOGIN_PASSWORD_ERROR));
+        Assert.assertEquals("Укажите пароль", textElementError.getText());
+        driver.quit();
+    }
+}
